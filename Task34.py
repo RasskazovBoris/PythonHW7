86f47cf3051a4e108d4ef181f0ace6dbfd5a46d7
Boris <rasskazovboris@mail.ru>
2023-06-02 16:06:28 +0400

    ver 2.0

--- Task34.py
+++ Task34.py
@@ -2,7 +2,7 @@ sing = input("Песня Пуха: ").split()
 result = []
 
 for i in range(len(sing)):
-    result.append(list(filter(lambda x: x in "а", sing[i])))
+    result.append(list(filter(lambda x: x in "уеыаоэяию", sing[i])))
 result = list([len(i) for i in result])
 acc = 0
 for i in result:
